6a17567a501a193d5c8082b340e772da9897f81a
jiezhou <zhoujie338@126.com>
2017-03-09 14:19:30 +0800

    Add comment for RuleRank's fields.

--- src/main/java/com/thoughtworks/devcloud/model/RuleRank.java
+++ src/main/java/com/thoughtworks/devcloud/model/RuleRank.java
@@ -19,18 +19,25 @@ public class RuleRank implements Serializable, Comparable<RuleRank> {
 
     private static final long serialVersionUID = 1L;
 
+    /** 排名: 根据counts 降序排列 **/
     private int rank;
 
+    /** 规则名称: 对应Table C_RULES的字段: NAME **/
     private String ruleName;
 
+    /** 问题级别: 对应Table C_RULES的字段: PRIORITY **/
     private Integer priority;
 
+    /** 问题分类: 对应Table C_RULES的字段: CATEGORY_BIG **/
     private String category;
 
+    /** 语言: 对应Table C_RULES的字段: LANGUAGE **/
     private String language;
 
+    /** 标签: 对应Table C_RULES的字段: SYSTEM_TAGS **/
     private String tag;
 
+    /** 次数: 统计出来的**/
     private Long counts;
 
     public RuleRank(String ruleName, Integer priority, String category, String language, String tag, Long counts) {
