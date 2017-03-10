17b64b4519186afb1617ec44ff9fee2b779cc2e6
jiezhou <zhoujie338@126.com>
2017-03-10 11:24:57 +0800

    Modify issue query logic.

--- src/main/java/com/thoughtworks/devcloud/repository/TJenkinsJobInfoRepository.java
+++ src/main/java/com/thoughtworks/devcloud/repository/TJenkinsJobInfoRepository.java
@@ -1,20 +1,16 @@
 package com.thoughtworks.devcloud.repository;
 
-import com.thoughtworks.devcloud.domain.CIssues;
-import com.thoughtworks.devcloud.model.RuleRank;
+import com.thoughtworks.devcloud.domain.TJenkinsJobInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 
 /**
  * Repository for table T_JENKINS_JOB_INFO.
  */
 @Repository
-public interface TJenkinsJobInfoRepository extends JpaRepository<CIssues, Long> {
+public interface TJenkinsJobInfoRepository extends JpaRepository<TJenkinsJobInfo, Long> {
 
     @Query("SELECT count(distinct tj.gitUrl) FROM TJenkinsJobInfo tj")
     Long countDistinctByGitUrl();
