16c94a76d641ac90ad04d23b16f638064ea91dd0
jiezhou <zhoujie338@126.com>
2017-03-19 23:13:03 +0800

    Modify biz logic for rule rank.

---
+++ src/main/java/com/thoughtworks/devcloud/repository/CSnapshotsRepository.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.devcloud.repository;
+
+import com.thoughtworks.devcloud.domain.CSnapshots;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+/**
+ * Repository for table C_SNAPSHOTS.
+ */
+@Repository
+public interface CSnapshotsRepository extends JpaRepository<CSnapshots, Long> {
+
+    @Query(value = "SELECT s1.id FROM C_SNAPSHOTS s1 " +
+            "JOIN" +
+            "(SELECT s2.scm_addr AS scm_addr, MAX(s2.COMMIT_END_TIME) AS end_time FROM C_SNAPSHOTS s2 " +
+            "GROUP BY s2.scm_addr) AS s3 " +
+            "ON s1.scm_addr = s3.scm_addr AND s1.commit_end_time = s3.end_time", nativeQuery = true)
+    List<Long> findLatestCSnapshotsIdListByGitUrl();
+}
