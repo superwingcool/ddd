e477e71a36b1d1f4d23bec05d0fc1090ca951e01
jiezhou <zhoujie338@126.com>
2017-03-23 23:19:11 +0800

    Add unit test.

--- src/test/java/com/thoughtworks/devcloud/constants/CIssueManualStatusFieldTest.java
+++ src/test/java/com/thoughtworks/devcloud/constants/CIssueManualStatusFieldTest.java
@@ -14,5 +14,15 @@ public class CIssueManualStatusFieldTest {
     public void shouldReturnAllStatusString() {
         List<Integer> manualStatusList = CIssueManualStatusField.getAllManualStatuses();
         Assert.assertEquals(2, manualStatusList.size());
+
+        Assert.assertTrue(manualStatusList.contains(0));
+        Assert.assertTrue(manualStatusList.contains(2));
+        Assert.assertFalse(manualStatusList.contains(1));
+    }
+
+    @Test
+    public void shouldReturnInteger() {
+        Assert.assertEquals(2, CIssueManualStatusField.IGNORED.value());
+        Assert.assertEquals(0, CIssueManualStatusField.UNIGNORED.value());
     }
 }
